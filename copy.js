01c3818f2d9b69200ef39cfcf2fff6ccc9ada26f
Steve Zelaznik <steve.zelaznik@iorahealth.com>
2016-09-09 13:50:53 -0500

    DeepCopy now handles duplicate objects inside the structure.

--- copy.js
+++ copy.js
@@ -1,12 +1,15 @@
-function passThrough(obj) {
+'use strict';
+
+function passThrough(obj, mem, level) {
     return obj;
 }
 
-function iterate(orig, level) {
+function iterate(orig, mem, level) {
+    level = level || 0;
     var dup = new orig.constructor();
     for (var key in orig) {
         var val = orig[key];
-        dup[key] = deepCopy(val, level+1);
+        dup[key] = deepCopy(val, mem, level + 1);
     }
     return dup;
 }
@@ -19,17 +22,56 @@ const types = Object.freeze({
     'object':    iterate
 });
 
-function deepCopy(obj, level) {
-    if (!obj)
-        return obj;
-    if (level >= 100)
-        throw new Error("max recursion level reached.");
+function findOrCreateCache(obj, mem, level) {
+    var k, i, tmpPair, pair, bucket, parsingErr;
 
-    var func = types[typeof(obj)];
+    try {
+        k = JSON.stringify(obj);
+    } catch(e) {
+        parsingErr = e;
+    }
+
+    if (k) {
+        bucket = mem.cache[k] = (mem.cache[k] || []);
+    } else {
+        bucket = mem.err;
+    }
+
+    for (i in bucket) {
+        tmpPair = bucket[i];
+        if (obj === tmpPair.orig) {
+            pair = tmpPair;
+            break;
+        }
+    }
+
+    if (pair === undefined) {
+        pair = {orig: obj};
+        bucket.push(pair);
+    }
+
+    return pair;
+}
+
+function deepCopy(obj, mem, level) {
+    var pair, func;
+    if (!obj) { return obj; }
+
+    level = level || 0;
+    mem = mem || {err: [], cache: {}};
+    pair = findOrCreateCache(obj, mem, level);
+
+    if (pair.copy) {
+        return pair.copy;
+    }
+
+    func = types[typeof(obj)];
     if (func === undefined)
         throw new Error("Unknown type: " + typeof(obj));
-    else
-        return func(obj, (level || 0) + 1);
+    else {
+        pair.copy = func(obj, mem, level + 1);
+        return pair.copy;
+    }
 }
 
 module.exports = deepCopy;
